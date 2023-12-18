1c87a9a69b0074e47db56c2d732148047c1b9195
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-12-18 13:53:13 -0600

    fixed the bug in get_by_id function

--- models/db_dao.py
+++ models/db_dao.py
@@ -3,12 +3,12 @@ from copy import copy
 from models.admin_dao import AdminDao
 from models.db_dao import Db as db
 from models.user_dao import UserDao
-from modules.book_dao import BookDao
+from models.book_dao import BookDao
 
 class DbDao(db):
-    def __init__(self):
+    def __init__(self, app):
         super(DbDao, self).__init__(app)
         
         self.book = BookDao(copy(self))
         self.user = UserDao(copy(self))
-        self.admin = AdminDao(copy(self
+        self.admin = AdminDao(copy(self))
